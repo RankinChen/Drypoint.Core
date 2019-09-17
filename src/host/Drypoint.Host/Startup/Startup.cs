53593740e2ca7b6814178100ad179dd4ac735361
陈浪林 <cll211@126.com>
2019-09-17 17:35:04 +0800

    修改路径和数据库链接(非重要提交)

--- src/host/Drypoint.Host/Startup/Startup.cs
+++ src/host/Drypoint.Host/Startup/Startup.cs
@@ -203,12 +203,12 @@ namespace Drypoint.Host.Startup
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
-                    name: "defaultWithArea",
-                    template: "{area}/{controller=Home}/{action=Index}/{id?}");
-
-                routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
+                    name: "defaultWithArea",
+                    template: "{area}/{controller=Home}/{action=Index}/{id?}");
             });
 
         }
