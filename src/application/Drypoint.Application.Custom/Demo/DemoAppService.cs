5db315ab90777c114ce6db03c8acef287be914c9
陈浪林 <cll211@126.com>
2019-10-14 16:11:09 +0800

    剔除多余验证。

--- src/application/Drypoint.Application.Custom/Demo/DemoAppService.cs
+++ src/application/Drypoint.Application.Custom/Demo/DemoAppService.cs
@@ -49,7 +49,6 @@ namespace Drypoint.Application.Custom.Demo
         /// <returns>无参 有返回值</returns>
         [HttpGet]
         [ApiExplorerSettings(GroupName = DrypointConsts.AppAPIGroupName)]
-        [Authorize()]
         [DrypointAuthorize("aaaaa", "BBBB")]
         public ListResultDto<DemoOutputDto> GetAll()
         {
